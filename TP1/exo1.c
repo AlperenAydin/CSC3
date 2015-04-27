f646d4e998d6eb562a47cfe478fc288151f4d1c9
AlperenAydin <aydin.alperen.bugra@gmail.com>
2015-04-27 13:30:46 +0200

    TP1

---
+++ TP1/exo1.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main (int argc, char* argl[])
+{
+  int i;
+  printf("Nom de l'executable: %s \n",argl[0]);
+  printf("Nom de l'executable: %s \n",agrc-1);
+  printf("Les arguments sont: ");
+  for( i=1; i<argc; i++)
+    {
+      printf("\t%d = %s \n ";i,argv[i]);
+    }
+
+  return 0; 
+}
