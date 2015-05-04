ae7c27fe85c8549067b97ccdc44e354c392944b3
AlperenAydin <aydin.alperen.bugra@gmail.com>
2015-05-04 17:34:19 +0200

    Better efficency

--- TP2/efficency.m
+++ TP2/efficency.m
@@ -1,16 +1,20 @@
 close all; clear all;
 
-N = 100;
+N = 25;
 t = zeros(1,N);
 
 for i = 1:N
-    i
-   command = sprintf( '(time ./fractal %d)2>&1|grep real',i); 
-   [status,cmdout] = system(command);
-   l = length(cmdout);
-   str = cmdout(l-6:l-2);
-   t(i)= sscanf(str,'%f');
-   
+   x = 0;
+   i
+   for j=1:10 
+       j
+       command = sprintf( '(time ./fractal %d)2>&1|grep real',i);
+       [status,cmdout] = system(command);
+       l = length(cmdout);
+       str = cmdout(l-6:l-2);
+       x = x +sscanf(str,'%f');
+   end
+   t(i) = x/10;
 end
 
 plot(t);
